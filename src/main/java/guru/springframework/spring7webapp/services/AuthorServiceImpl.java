9059df95a4668db15a4b48004145a9a253d4805f
jhuynh <johnhuynh238@gmail.com>
2026-05-13 20:55:53 -0700

    Add Thymeleaf views, controllers and services
    Add Thymeleaf dependency and basic MVC stack for authors and books. Introduces AuthorController and BookController, service interfaces and implementations (AuthorService/AuthorServiceImpl, BookService/BookServiceImpl) that delegate to repositories, and Thymeleaf templates (authors.html, books.html) to render lists. Enables simple web pages for browsing authors and books.

---
+++ src/main/java/guru/springframework/spring7webapp/services/AuthorServiceImpl.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring7webapp.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.spring7webapp.domain.Author;
+import guru.springframework.spring7webapp.repositories.AuthorRepository;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public Iterable<Author> findAll() {
+        return authorRepository.findAll();
+    }
+}
